66e9f11bef24ba6941c97050e653b28fe4709a9d
Xcyberex <96276503+Hackexdecodebreaker@users.noreply.github.com>
2026-08-29 23:35:44 +0000

    Update bypass.sh

--- bypass/bypass.sh
+++ bypass/bypass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Security bypass techniques enumeration script
 # Author: Hackexdecodebreaker (https://github.com/Hackexdecodebreaker)
-# WARNING: For authorized testing only
+
 
 figlet -f slant "BYPASS"
 toilet -f mono12 -F metal "Author: Hackexdecodebreaker"
