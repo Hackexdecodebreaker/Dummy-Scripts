148fccb1a037dcc6ffbaa644dd72ac0cc6992a8d
Xcyberex <96276503+Hackexdecodebreaker@users.noreply.github.com>
2026-08-29 23:36:07 +0000

    Update ipblock_bypass.sh

--- bypass/ipblock_bypass.sh
+++ bypass/ipblock_bypass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # IP Blocking Bypass via Header Manipulation
 # Author: Hackexdecodebreaker (https://github.com/Hackexdecodebreaker)
-# WARNING: For authorized testing only
+
 
 figlet -f slant "IPBLOCK BYPASS"
 toilet -f mono12 -F metal "Author: Hackexdecodebreaker"
